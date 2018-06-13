7a9d7e721bc865ad198a4999efa368b791cf6d83
Christian Fobel <christian@fobel.net>
2018-06-13 13:33:54 -0400

    feat(init): initial commit

---
+++ setup.py
@@ -0,0 +1,226 @@
+# See: http://www.py2exe.org/index.cgi/win32com.shell?action=show&redirect=WinShell
+# ModuleFinder can't handle runtime changes to __path__, but win32com uses them
+try:
+    # py2exe 0.6.4 introduced a replacement modulefinder.
+    # This means we have to add package paths there, not to the built-in
+    # one.  If this new modulefinder gets integrated into Python, then
+    # we might be able to revert this some day.
+    # if this doesn't work, try import modulefinder
+    try:
+        import py2exe.mf as modulefinder
+    except ImportError:
+        import modulefinder
+    import win32com, sys
+    for p in win32com.__path__[1:]:
+        modulefinder.AddPackagePath("win32com", p)
+    for extra in ["win32com.shell"]: #,"win32com.mapi"
+        __import__(extra)
+        m = sys.modules[extra]
+        for p in m.__path__[1:]:
+            modulefinder.AddPackagePath(extra, p)
+except ImportError:
+    # no build path setup, no worries.
+    pass
+
+
+from distutils.core import setup
+import itertools as it
+import os
+import runpy
+import site
+
+from py2exe.build_exe import py2exe as build_exe
+import jsonschema
+import microdrop
+import gst
+import gtk
+import notebook
+import nbformat
+import path_helpers as ph
+import pint
+import matplotlib
+import zmq
+import whichcraft
+
+
+def walk_dll(dll_name):
+    for p in [ph.path(p).expand() for p in os.environ['PATH'].split(';')]:
+        if p.isdir():
+            for f in p.files(dll_name):
+                yield f
+
+
+site_packages = ph.path([p for p in site.getsitepackages()
+                         if p.endswith('site-packages')][0])
+
+for p in (('pyutilib', ), ('pyutilib', 'component')):
+    init_i = site_packages.joinpath(*(p + ('__init__.py', )))
+    if not init_i.isfile():
+        print 'Created missing `%s`.' % init_i
+        init_i.touch()
+
+
+class JsonSchemaCollector(build_exe):
+    """
+    This class Adds jsonschema files draft3.json and draft4.json to
+    the list of compiled files so it will be included in the zipfile.
+    """
+    def copy_extensions(self, extensions):
+        build_exe.copy_extensions(self, extensions)
+        collect_dir = ph.path(self.collect_dir)
+
+        for path_i, module_i in ((ph.path(notebook.__path__[0]),
+                                  ('templates', )),
+                                 (ph.path(jsonschema.__path__[0]),
+                                  ('schemas', )),
+                                 (ph.path(nbformat.__path__[0]),
+                                  ('v4', )),
+                                 (ph.path(pint.__path__[0]),  # pint data files
+                                  ('', )),
+                                 (ph.path(microdrop.__path__[0]),  # microdrop glade files, icon
+                                  ('', ))):
+            data_path = path_i.joinpath(*module_i)
+
+            # Copy the template files to the collection dir. Also add the copied
+            # file to the list of compiled files so it will be included in the
+            # zipfile.
+            # self.collect_dir.joinpath(rel_path_i).makedirs_p()
+
+            for file_j in data_path.walkfiles():
+                if file_j.ext.startswith('.py'):
+                    continue
+                rel_path_j = path_i.parent.relpathto(file_j)
+                collected_j = collect_dir.joinpath(rel_path_j)
+                collected_j.parent.makedirs_p()
+                self.copy_file(file_j, collected_j)
+                self.compiled_files.append(rel_path_j)
+
+
+# libzmq.dll is in same directory as zmq's __init__.py
+
+os.environ["PATH"] += (os.path.pathsep +
+                       os.path.pathsep.join([os.path.split(zmq.__file__)[0],
+                                             # GStreamer DLLs
+                                             ph.path(gst.__file__).parent
+                                             .joinpath('plugins'),
+                                             ph.path(gst.__file__).parent
+                                             .joinpath('bin')]))
+
+
+def data_files():
+    '''
+    Collect data files for packages that are not supported out of the box.
+    '''
+    # Seems like `libzmq.pyd` needs to be copied to `dist` directory, *even
+    # though* it is already automatically copied by `py2exe` to the name
+    # `zmq.libzmq.pyd`.
+    # data_files_ = [('', [ph.path(zmq.__path__[0]).joinpath('libzmq.pyd')])]
+    data_files_ = [('', ph.path(gst.__path__[0]).joinpath('bin').files('*.dll'))]
+
+    # Jupyter notebook templates and static files cannot be accessed within the
+    # zip file, so need to be copied.
+    # The `nbformat` package contains a `jsonschema` file which cannot be
+    # accessed within the zip file, so it also needs to be copied.
+    for path_i, module_i in ((ph.path(notebook.__path__[0]), ('templates', )),
+                             (ph.path(notebook.__path__[0]), ('static', )),
+                             (ph.path(nbformat.__path__[0]), tuple())):
+        data_path = path_i.joinpath(*module_i)
+
+        # Copy the template files to the collection dir. Also add the copied
+        # file to the list of compiled files so it will be included in the
+        # zipfile.
+        files = sorted([file_j for file_j in data_path.walkfiles()])
+        for parent_i, files_i in it.groupby(files, lambda x: x.parent):
+            data_files_ += [(path_i.parent.relpathto(parent_i),
+                             list(files_i))]
+
+    # Add Intel Math Kernel Library DLLs for `numpy`, `pandas`, etc.
+    data_files_ += [('', list(walk_dll('mkl_*.dll')) +
+                     list(walk_dll('MSVCP90.dll')) +
+                     list(walk_dll('libiomp5md.dll')))]
+
+    # gtk theme
+    runtime_path = ph.path(gtk.__file__).parent.parent.joinpath('gtk_runtime')
+    rc_path = runtime_path.joinpath('share', 'themes', 'MS-Windows', 'gtk-2.0',
+                                    'gtkrc')
+    engines_path = runtime_path.joinpath('lib', 'gtk-2.0', '2.10.0', 'engines')
+    icons_path = runtime_path.joinpath('share', 'icons', 'hicolor')
+    data_files_ += [(r'etc/gtk-2.0', [rc_path]),
+                    (runtime_path.relpathto(engines_path),
+                     list(engines_path.files('*.dll'))),
+                    (runtime_path.relpathto(icons_path),
+                     list(icons_path.walkfiles()))]
+    return data_files_
+
+
+runpy_file = os.path.join(os.path.split(runpy.__file__)[0], 'runpy.py')
+# microdrop_file = ph.path(microdrop.__file__).parent.joinpath('microdrop.py')
+microdrop_file = ph.path(whichcraft.which('microdrop')).parent.joinpath('microdrop-script.py')
+pip_file = ph.path(whichcraft.which('pip')).parent.joinpath('pip-script.py')
+
+setup(console=['jupyter-notebook.py', 'ipython.py', runpy_file] +
+      map(str, (microdrop_file, pip_file)),
+      cmdclass={"py2exe": JsonSchemaCollector},
+      # See http://www.py2exe.org/index.cgi/ListOfOptions
+      options={'py2exe': {'unbuffered': True,
+                          'excludes': ['jinja2.asyncsupport'],
+# Work around 'api-ms-win-core-registry-l1-1-0.dll' not found:
+# https://stackoverflow.com/a/40090641/345236
+"dll_excludes": [
+'MSVCP90.dll',
+'IPHLPAPI.DLL',
+'NSI.dll',
+'WINNSI.DLL',
+'WTSAPI32.dll',
+'SHFOLDER.dll',
+'PSAPI.dll',
+'MSVCR120.dll',
+'MSVCP120.dll',
+'CRYPT32.dll',
+'GDI32.dll',
+'ADVAPI32.dll',
+'CFGMGR32.dll',
+'USER32.dll',
+'POWRPROF.dll',
+'MSIMG32.dll',
+'WINSTA.dll',
+'MSVCR90.dll',
+'KERNEL32.dll',
+'MPR.dll',
+'Secur32.dll',
+],
+                          # 'dll_excludes': ['MSVFW32.dll',
+                                           # 'AVIFIL32.dll',
+                                           # 'AVICAP32.dll',
+                                           # 'ADVAPI32.dll',
+                                           # 'CRYPT32.dll',
+                                           # 'WLDAP32.dll'],
+                          'includes': ['matplotlib', 'numpy', 'pandas',
+                                       'matplotlib.backends.backend_wxagg',
+                                       'cycler', 'IPython',
+                                       'cairo', 'gio', 'pango', 'pangocairo', 'atk',  # gtk
+                                       'pyutilib.component.core', 'pyutilib.component.config', 'pyutilib.component.loader',  # pyutilib
+                                       # microdrop
+                                       'lxml.etree',
+                                       'lxml._elementpath',
+                                       'win32com.shell',
+                                       'pkg_resources.py31compat',
+                                       'pkg_resources._vendor.appdirs',
+                                       'pkg_resources._vendor.packaging.markers',
+                                       'pkg_resources._vendor.packaging.requirements',
+                                       'pkg_resources._vendor.packaging.specifiers',
+                                       'pkg_resources._vendor.packaging.utils',
+                                       'pkg_resources._vendor.packaging.version',
+                                       'pkg_resources._vendor.packaging._compat',
+                                       'pkg_resources._vendor.packaging._structures',
+                                       'pkg_resources._vendor.packaging.__about__',
+                                       'pkg_resources._vendor.pyparsing',
+                                       'pkg_resources._vendor.six',
+
+                                       'zmq',
+                                       'zmq.utils',
+                                       'zmq.utils.jsonapi',
+                                       'zmq.utils.strtypes']}},
+      # See http://www.py2exe.org/index.cgi/MatPlotLib
+      data_files=matplotlib.get_py2exe_datafiles() +
+      data_files())
