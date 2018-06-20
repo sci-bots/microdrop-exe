de5f6e631aad993286cec7fc582a099d4570ea61
Christian Fobel <christian@fobel.net>
2018-06-20 14:57:41 -0400

    build(appveyor): add appveyor build configuration

---
+++ .appveyor/build.ps1
@@ -0,0 +1,33 @@
+Set-PSDebug -Trace 1
+Set-ExecutionPolicy RemoteSigned
+
+# Set version number based on git tag
+$x = git describe --tags
+if (!$?){ $prevTag="v0.0-0"
+} else  { $prevTag = $x.Split("-")[0] + "-" + $x.Split("-")[1] }
+
+$buildTag = $prevTag + "+" + $(Get-Date -Format FileDateTime)
+Write-Host "Build Tag: $buildTag"
+Update-AppveyorBuild -Version $buildTag
+
+activate.ps1 $env:APPVEYOR_PROJECT_NAME
+
+conda env create --file environment-template.yaml
+
+# Link all available plugins to enabled directory
+python -m mpm.bin.api enable $(dir $env:CONDA_PREFIX\share\microdrop\plugins\available)
+
+# Build packaged MicroDrop Windows executable application.
+python .\setup.py py2exe
+
+# Rename `dist` directory to `microdrop-<version>`.
+$dist_dir = "microdrop-$($x.Substring(1))"
+mv dist $dist_dir
+
+# Create self-extracting 7zip archive
+7za a -sfx"7z.sfx" -r "$dist_dir.7z" $dist_dir
+
+# Prepend platform (i.e., `win-32`, `win-64`, `noarch`) to each package filename
+# and collect package files in `artifacts` directory.
+md artifacts
+mv "$dist_dir.7z" artifacts
---
+++ .appveyor/install.ps1
@@ -0,0 +1,31 @@
+Set-PSDebug -Trace 1
+Set-ExecutionPolicy RemoteSigned
+
+# Configure Conda to operate without user input
+conda config --set always_yes yes --set changeps1 no
+
+# Allow extra Conda channels to be added (e.g., for testing).
+if ($env:CONDA_EXTRA_CHANNELS) {
+    $env:CONDA_EXTRA_CHANNELS.Split(";") | ForEach {
+        Write-Host "Adding Conda channel from %CONDA_EXTRA_CHANNELS%: $_"
+        conda config --add channels $_
+    }
+}
+
+
+# Use PSCondaEnvs to allow activation using powershell:
+conda install -n root -c pscondaenvs pscondaenvs
+
+# Update conda, and install conda-build (used for building in non-root env)
+conda update -q conda
+conda install --yes conda-build anaconda-client nose
+
+# Create new project environment
+conda env create --name $env:APPVEYOR_PROJECT_NAME environment.yaml
+
+activate.ps1 $env:APPVEYOR_PROJECT_NAME
+conda install 7za -y -c conda-forge
+
+# Download 7zip installer and extract _self-extracting (SFX)_ plugins.
+cmd /C curl -L --output 7zip-installer.exe https://www.7-zip.org/a/7z1805.exe
+7za x "-ir!*.sfx" ..\7zip-installer.exe
