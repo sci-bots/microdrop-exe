5635bef9b0df44ff0f3a11addcd70ea41a46c4f7
Christian Fobel <christian@fobel.net>
2018-06-20 17:19:28 -0400

    fix(pyutilib): backwards compatibility for < 5.0
    As of `pyutilib` version 5.0, `pyutilib.component.core.PluginGlobals` does
    not have a static function `push_env`.  The `push_env` function has been
    replaced by `add_env`.
    
    In this commit, add a shim in the `custom_boot_script.py` to add
    `push_env` as an alias for `add_env` if `push_env` is not found.

--- custom_boot_script.py
+++ custom_boot_script.py
@@ -1,8 +1,14 @@
 import os
 import sys
 
+from pyutilib.component.core import PluginGlobals
 import path_helpers as ph
 
+
+# Add backwards compatibility for `pyutilib<5.0`
+if not hasattr(PluginGlobals, 'push_env') and \
+        hasattr(PluginGlobals, 'add_env'):
+    PluginGlobals.push_env = PluginGlobals.add_env
 
 MICRODROP_PYTHONPATH = [ph.path(p)
                         for p in os.environ.get('MICRODROP_PYTHONPATH',
