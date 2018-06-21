343b48f3d8f88ea00ee63f769782ab01667739fa
Christian Fobel <christian@fobel.net>
2018-06-21 12:37:59 -0400

    feat(patch): add function to apply patches
    In this commit, search for patches to apply in `patches` directory, where
    the first line of each patch file is path to file to be patched, relative
    to the `sys.prefix` directory.
    
    Skip patches if they have already been applied.

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ import os
 import pkg_resources
 import runpy
 import site
+import subprocess as sp
 import sys
 
 from py2exe.build_exe import py2exe as build_exe
@@ -77,6 +78,36 @@ def resource_modules(root_module):
                        pkg_resources.resource_isdir(root_module, p) and
                        p.ext.startswith('.py')]))
 
+
+def apply_patches(patches_dir):
+    '''
+    Apply patches in specified directory.
+
+    First line of each patch file is path to file to be patched, relative to
+    the :attr:`sys.prefix` directory.
+
+    Skip patches if they have already been applied.
+    '''
+    sys_prefix = ph.path(sys.prefix)
+    patches_dir = ph.path(patches_dir).realpath()
+    if not patches_dir.isdir():
+        return
+    for patch_file_i in patches_dir.files():
+        file_path_i = sys_prefix.joinpath(patch_file_i.lines()[0]
+                                          .strip()).realpath()
+        if not file_path_i.isfile():
+            print >> sys.stderr, 'File to patch not found: `%s`' % file_path_i
+            continue
+        process_i = sp.Popen(['patch', '-d', file_path_i.parent,
+                              file_path_i.name, '-i', patch_file_i,
+                              '-t',  # Ask no questions
+                              '-N',  # Ignore patches that appear to be
+                                     # reversed or already applied.
+                             ], shell=True)
+        process_i.wait()
+
+
+apply_patches('patches')
 
 site_packages = ph.path([p for p in site.getsitepackages()
                          if p.endswith('site-packages')][0])
