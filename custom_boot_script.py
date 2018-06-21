535153a910c344a681fddae11e119307476c9191
Christian Fobel <christian@fobel.net>
2018-06-20 20:38:10 -0400

    build(microdrop): bump to 2.22.2

--- custom_boot_script.py
+++ custom_boot_script.py
@@ -1,14 +1,8 @@
 import os
 import sys
 
-from pyutilib.component.core import PluginGlobals
 import path_helpers as ph
 
-
-# Add backwards compatibility for `pyutilib<5.0`
-if not hasattr(PluginGlobals, 'push_env') and \
-        hasattr(PluginGlobals, 'add_env'):
-    PluginGlobals.push_env = staticmethod(PluginGlobals.add_env)
 
 MICRODROP_PYTHONPATH = [ph.path(p)
                         for p in os.environ.get('MICRODROP_PYTHONPATH',
