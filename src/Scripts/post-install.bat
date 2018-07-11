642cea2226c270ac60205bec4318432456c79d89
Christian Fobel <christian@fobel.net>
2018-07-11 14:51:25 -0400

    chore(device-ui): hide connection layer by default

--- src/Scripts/post-install.bat
+++ src/Scripts/post-install.bat
@@ -6,3 +6,5 @@ REM chore(plugins): enable all plugins by default
 %~dp0\microdrop-config edit --append plugins.enabled step_label_plugin
 REM perf(logging): set log level to `warning` to reduce logging overhead
 %~dp0\microdrop-config edit --set microdrop\.app.log_level warning
+REM chore(device-ui): hide connections layer by default
+%~dp0\microdrop-config edit --set dmf_device_ui_plugin.surface_alphas "{""background"":1.0,""shapes"":1.0,""connections"":0.0,""routes"":1.0,""channel_labels"":1.0,""actuated_shapes"":1.0,""registration"":0.0}"
