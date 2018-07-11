d8e4ae8c3ef8d93be58bd159594e01e94d9359dc
Christian Fobel <christian@fobel.net>
2018-07-11 14:46:58 -0400

    perf(logging): set default log level to `warning`
    Prior to this commit, running MicroDrop with the logging level
    set to `info` was **much** slower when running from the bundled `.exe`
    versus from a development environment (i.e., `python -m microdrop.microdrop`).
    This seems to mostly be related to logging speed, especially from the
    `dropbot_plugin` where the majority of `info` level logging messages come
    from.
    
    In this commit, as a workaround, reduce logging overhead by setting the
    default log level to `warning` in the post-installation script.

--- src/Scripts/post-install.bat
+++ src/Scripts/post-install.bat
@@ -1,5 +1,8 @@
+REM chore(plugins): enable all plugins by default
 %~dp0\microdrop-config edit --append plugins.enabled droplet_planning_plugin
 %~dp0\microdrop-config edit --append plugins.enabled dmf_device_ui_plugin
 %~dp0\microdrop-config edit --append plugins.enabled dropbot_plugin
 %~dp0\microdrop-config edit --append plugins.enabled user_prompt_plugin
 %~dp0\microdrop-config edit --append plugins.enabled step_label_plugin
+REM perf(logging): set log level to `warning` to reduce logging overhead
+%~dp0\microdrop-config edit --set microdrop\.app.log_level warning
