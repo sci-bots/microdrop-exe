9142f0410c9dccdb9dd33a07bf82711aa022aa2c
Christian Fobel <christian@fobel.net>
2018-08-22 12:48:36 -0400

    chore(protocol-grid): set default column order
    Prior to this commit, the order of the columns in the protocol grid was
    undefined on first launch of MicroDrop.
    
    In this commit, set the default protocol grid column order to the
    following after initial installation:
    
    ```python
    columns = [
    'Id',
    'Schema',
    'Duration (s)',
    'Voltage (v)',
    'Repeat duration s',
    'Route repeats',
    'Trail length',
    'Message',
    'Volume threshold',
    'Frequency (hz)',
    'Video',
    ]
    ```
    
    Note
    ----
    
    The following Python snippet can be used to print out a command to set the
    column orders for inclusion in the `post-install.bat` script:
    
    ```python
    print '%%~dp0\microdrop-config --set microdrop\.gui\.protocol_grid_controller.column_positions "%s"' % str({c: i for i, c in enu ... merate(columns)}).replace("'", '""')
    ```
    
    For the column ordering used in this commit, the above snippet produces:
    
    ```sh
    %~dp0\microdrop-config --set microdrop\.gui\.protocol_grid_controller.column_positions "{""Trail length"": 6, ""Message"": 7, ""Id"": 0, ""Duration (s)"": 2, ""Frequency (hz)"": 9, ""Video"": 10, ""Voltage (v)"": 3, ""Volume threshold"": 8, ""Repeat duration s"": 4, ""Route repeats"": 5, ""Schema"": 1}"
    ```

--- src/Scripts/post-install.bat
+++ src/Scripts/post-install.bat
@@ -7,4 +7,6 @@ REM chore(plugins): enable all plugins by default
 REM perf(logging): set log level to `info` to reduce logging overhead
 %~dp0\microdrop-config edit --set microdrop\.app.log_level info
 REM chore(device-ui): hide connections layer by default
-%~dp0\microdrop-config edit --set dmf_device_ui_plugin.surface_alphas "{""background"":1.0,""shapes"":1.0,""connections"":0.0,""routes"":1.0,""channel_labels"":1.0,""actuated_shapes"":1.0,""registration"":0.0}"
+%~dp0\microdrop-config edit --set dmf_device_ui_plugin.surface_alphas "{""background"":1.0,""shapes"":1.0,""connections"":0.0,""routes"":1.0,""channel_labels"":1.0,""static_electrode_state_shapes"":0.3,""dynamic_electrode_state_shapes"":0.7,""registration"":0.0}"
+REM chore(protocol-grid-controller): set default column positions
+%~dp0\microdrop-config edit --set microdrop\.gui\.protocol_grid_controller.column_positions "{""Trail length"": 6, ""Message"": 7, ""Id"": 0, ""Duration (s)"": 2, ""Frequency (hz)"": 9, ""Video"": 10, ""Voltage (v)"": 3, ""Volume threshold"": 8, ""Repeat duration s"": 4, ""Route repeats"": 5, ""Schema"": 1}"
