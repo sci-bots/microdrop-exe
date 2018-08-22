0fc78ad046fa3058ee8f997479f2c4a1837435dd
Christian Fobel <christian@fobel.net>
2018-08-22 12:48:10 -0400

    chore(logging): set default log level to info

--- src/Scripts/post-install.bat
+++ src/Scripts/post-install.bat
@@ -4,7 +4,7 @@ REM chore(plugins): enable all plugins by default
 %~dp0\microdrop-config edit --append plugins.enabled dropbot_plugin
 %~dp0\microdrop-config edit --append plugins.enabled user_prompt_plugin
 %~dp0\microdrop-config edit --append plugins.enabled step_label_plugin
-REM perf(logging): set log level to `warning` to reduce logging overhead
-%~dp0\microdrop-config edit --set microdrop\.app.log_level warning
+REM perf(logging): set log level to `info` to reduce logging overhead
+%~dp0\microdrop-config edit --set microdrop\.app.log_level info
 REM chore(device-ui): hide connections layer by default
 %~dp0\microdrop-config edit --set dmf_device_ui_plugin.surface_alphas "{""background"":1.0,""shapes"":1.0,""connections"":0.0,""routes"":1.0,""channel_labels"":1.0,""actuated_shapes"":1.0,""registration"":0.0}"
