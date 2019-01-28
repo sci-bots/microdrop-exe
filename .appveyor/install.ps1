2660acaa40ed5000c7b5554dc09b789898134bcc
Christian Fobel <christian@fobel.net>
2019-01-28 16:08:58 -0500

    fix(appveyor): open RDP session for debug

--- .appveyor/install.ps1
+++ .appveyor/install.ps1
@@ -23,10 +23,13 @@ conda update -q conda
 conda env create --name $env:APPVEYOR_PROJECT_NAME --file environment.yaml
 if ($LASTEXITCODE) { throw "Failed to create build Conda environment." }
 
+activate.ps1
 activate.ps1 $env:APPVEYOR_PROJECT_NAME
 conda install 7za -y -c conda-forge
 
 # Download 7zip installer and extract _self-extracting (SFX)_ plugins.
 cmd /C curl -L --output 7zip-installer.exe https://www.7-zip.org/a/7z1805.exe
 7za x "-ir!*.sfx" 7zip-installer.exe
+$blockRdp = $true;
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
 if ($LASTEXITCODE) { throw "Failed to extract 7zip self-extracting executable module" }
