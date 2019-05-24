93646e3dc5c5f873fdfd3d54c02986e50ddcba78
Christian Fobel <christian@fobel.net>
2019-05-24 12:58:16 -0400

    fix(post-install): fix post-install script
    Prior to this commit, the post-install script contained a syntax error
    in the command intended to enable all installed MicroDrop plugins by
    default.  Apart from not enabling installed plugins by default, this
    prevented the rest of the post-install script from running as well.
    
    In this commit, fix the post-install command for enabling all installed
    plugins.

--- src/post-install.bat
+++ src/post-install.bat
@@ -1,8 +1,8 @@
 REM chore(plugins): enable all plugins by default
-for /f "usebackq delims=|" %f in (`dir /b "%~dp0\etc\microdrop\plugins\enabled"`) do %~dp0\microdrop-config edit --append plugins.enabled %f
+for /f "usebackq delims=|" %%f in (`dir /b "%~dp0etc\microdrop\plugins\enabled"`) do "%~dp0microdrop-config" edit --append plugins.enabled %%f
 REM perf(logging): set log level to `info` to reduce logging overhead
-%~dp0\microdrop-config edit --set microdrop\.app.log_level info
+%~dp0microdrop-config edit --set microdrop\.app.log_level info
 REM chore(device-ui): hide connections layer by default
-%~dp0\microdrop-config edit --set dmf_device_ui_plugin.surface_alphas "{""background"":1.0,""shapes"":1.0,""connections"":0.0,""routes"":1.0,""channel_labels"":1.0,""static_electrode_state_shapes"":0.7,""dynamic_electrode_state_shapes"":0.7,""registration"":0.0}"
+%~dp0microdrop-config edit --set dmf_device_ui_plugin.surface_alphas "{""background"":1.0,""shapes"":1.0,""connections"":0.0,""routes"":1.0,""channel_labels"":1.0,""static_electrode_state_shapes"":0.7,""dynamic_electrode_state_shapes"":0.7,""registration"":0.0}"
 REM chore(protocol-grid-controller): set default column positions
-%~dp0\microdrop-config edit --set microdrop\.gui\.protocol_grid_controller.column_positions "{""Trail length"": 6, ""Message"": 7, ""Id"": 0, ""Duration (s)"": 2, ""Frequency (hz)"": 9, ""Video"": 10, ""Voltage (v)"": 3, ""Volume threshold"": 8, ""Repeat duration s"": 4, ""Route repeats"": 5, ""Schema"": 1}"
+%~dp0microdrop-config edit --set microdrop\.gui\.protocol_grid_controller.column_positions "{""Trail length"": 6, ""Message"": 7, ""Id"": 0, ""Duration (s)"": 2, ""Frequency (hz)"": 9, ""Video"": 10, ""Voltage (v)"": 3, ""Volume threshold"": 8, ""Repeat duration s"": 4, ""Route repeats"": 5, ""Schema"": 1}"
